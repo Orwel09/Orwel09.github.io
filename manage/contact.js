a0a8181de16789ca279d946cbf2e6ebd7fa6f82f
王勇强 <849636243@qq.com>
2017-04-10 15:03:40 +0800

    联系我们

--- manage/contact.js
+++ manage/contact.js
@@ -7,6 +7,7 @@
 
     };
     function paging(){
+        $("#interesting").val('');
         var application = new Vue({
             el:"#contact",
             data:{
@@ -29,7 +30,42 @@
         })
     }
     function bindLabelEventInit(){
-
+        var mail = "";name = "";country = "";company = "";phone = "";city = "";interesting="";information = {};
+        $("#submit").on("click",function(){
+            mail = $("#mail").val();
+            name = $("#name").val();
+            country = $("#country").val();
+            company = $("#company").val();
+            phone = $("#phone").val();
+            city = $("#city").val();
+            interesting = $("#interesting").val();
+            if(mail==""||mail.indexOf("@")==-1){
+                alert("请输入正确的电子邮箱");
+                return;
+            }
+            if(name==""){
+                alert("请输入姓名");
+                return;
+            }
+            if(country==""){
+                alert("请输入国家");
+                return;
+            }
+            if(company==""){
+                alert("请输入您的公司名");
+                return;
+            }
+            information = {
+                mail:mail,
+                name:name,
+                country:country,
+                company:company,
+                phone:phone,
+                city:city,
+                interesting:interesting
+            };
+            alert(JSON.stringify(information));
+        })
     }
     function onEnter () {
         bindLabelEventInit ();

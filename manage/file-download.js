171de0003cb6fda361639a5536495a8347f6745a
王勇强 <849636243@qq.com>
2017-04-10 11:08:10 +0800

    新菜单

---
+++ manage/file-download.js
@@ -0,0 +1,39 @@
+/**
+ * Created by wangyongqiang on 17/4/6.
+ */
+(function(){
+    //页面初始化
+    window.onload = function(){
+
+    };
+    function paging(){
+        var application = new Vue({
+            el:"#file-download",
+            data:{
+            },
+            methods:{
+                showMenu:function(){
+                    var e = event.target;
+                    if($(e).next().css("display")=='none'){
+                        $(".secondMenu").css("display","none");
+                        $(e).next().css("display","block");
+                    }else{
+                        $(".secondMenu").css("display","none");
+                        $(e).next().css("display","none");
+                    }
+                },
+                downMenu:function(){
+                    $(".secondMenu").css("display","none");
+                }
+            }
+        })
+    }
+    function bindLabelEventInit(){
+
+    }
+    function onEnter () {
+        bindLabelEventInit ();
+        paging ();
+    }
+    onEnter();
+})();
